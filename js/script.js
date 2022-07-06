b5148ae1fcaeb75e43fcc3a535b5d298ed45f7f6
Ilya Kracvhyk <ilia_krav4yk@mail.ru>
2022-07-06 22:25:38 +0300

    Homw Work 1

---
+++ js/script.js
@@ -0,0 +1,91 @@
+console.log('Задание №2');
+let a1 = 5 % 3;
+a2 = 3 % 5;
+a3 = 5 + '3';
+a4 = '5' - 3;
+a5 = 75 + 'кг';
+a6 = 785 * 653;
+a7 = 100 / 25;
+a8 = 0 * 0;
+a9 = 0 / 2;
+a10 = 89 / 0;
+a11 = 98 + 2;
+a12 = 5 - 98;
+a13 = (8 + 56 * 4) / 5;
+a14 = (9 - 12) * 7 / (5 + 2);
+a15 = +"123";
+a16 = 1 || 0;
+a17 = false || true;
+a18 = true > 0;
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+console.log(a6);
+console.log(a7);
+console.log(a8);
+console.log(a9);
+console.log(a10);
+console.log(a11);
+console.log(a12);
+console.log(a13);
+console.log(a14);
+console.log(a15);
+console.log(a16);
+console.log(a17);
+console.log(a18);
+console.log('--------------------------------------------');
+/*------------------------------------------------------------------------------------------------------------*/
+console.log('Задание №4');
+let cylinderHeight = 10;
+let cylinderDiametr = a7;
+let p = 3.14;
+let VCilindra = p * (cylinderDiametr / 2) ** 2 * cylinderHeight;
+console.log(VCilindra);
+console.log('--------------------------------------------');
+/*---------------------------------------------------------------------------------------------------------------*/
+console.log('Задание №3')
+let width = 10;
+let height = 23;
+let SPyram = width * height;
+console.log(SPyram);
+console.log('--------------------------------------------');
+/*-------------------------------------------------------------------*/
+console.log('Задание №5')
+let RKruga = 5;
+//p = 3.14 инициализирован выше
+let SKruga = p * RKruga ** 2;
+console.log(SKruga);
+console.log('--------------------------------------------');
+/*---------------------------------------------------------*/
+console.log('Задание №6')
+let O1Trap = 5;
+let O2Trap = 7;
+let HTrap = 10;
+let STrap = ((O1Trap + O2Trap) / 2) * HTrap;
+console.log(STrap);
+console.log('--------------------------------------------');
+/*--------------------------------------------------------*/
+console.log('Задание №7')
+let sumCredit = 2;
+let percentYear = 10;
+let years = 5;
+let Pereplata = percentYear * years * 2 / 100;
+console.log(Pereplata + " млн.");
+console.log('--------------------------------------------');
+/*------------------------------*/
+console.log('Задание №8')
+let a = 8;
+let b = 3;
+let x1 = (16 + 2 * b - a) / 2
+console.log(x1);
+let x2 = (15 * b - a) / (6 - b)
+console.log(x2);
+let x3 = 23780 / (3 + a + b);
+console.log(x3);
+console.log('--------------------------------------------');
+
+
+
+
